16b0cc147bbf8bcd02bfe2a905fb2928e72ae0a7
SanecZ93 <zyryanovalex93@gmail.com>
2022-09-14 12:57:13 +0300

    Настройка кодировки

--- src/main/java/ru/job4j/oop/Encoding.java
+++ src/main/java/ru/job4j/oop/Encoding.java
@@ -2,6 +2,6 @@ package ru.job4j.oop;
 
 public class Encoding {
     public static void main(String[] args) {
-        System.out.println("текст");
+        System.out.println("РўРµРєСЃС‚");
     }
 }
