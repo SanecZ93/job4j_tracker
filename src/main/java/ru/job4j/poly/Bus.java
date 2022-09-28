0fd632833b8d3c1e62f8b3d14bb81e79155bd5c4
SanecZ93 <zyryanovalex93@gmail.com>
2022-09-28 19:05:57 +0300

    3. Интерфейс Input [#181092 #347001]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,12 +3,11 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-        System.out.println("going to town");
     }
 
     @Override
-    public int passenger(String question) {
-        return 30;
+    public void passenger() {
+        System.out.println(24);
     }
 
     @Override
--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -3,7 +3,7 @@ package ru.job4j.poly;
 public interface Transport {
     void drive();
 
-    public int passenger(String question);
+    void passenger();
 
     public int fuel(String question);
 }
