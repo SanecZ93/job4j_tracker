abfa91fe323d6e736a24f35dd5225ebb6ebe2cec
SanecZ93 <zyryanovalex93@gmail.com>
2022-09-28 08:13:36 +0300

    2. Scanner и чтение числа из консоли. [#179518 #346402]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -10,21 +10,21 @@ public class Matches {
         boolean turn = true;
         int count = 11;
         while (count > 0) {
-        String player = turn ? "Первый игрок" : "Второй игрок";
-        System.out.println(player + " введите число от 1 до 3:");
-        int matches = Integer.parseInt(input.nextLine());
-        if (matches >= 1 && matches <= Math.min(3, count)) {
-            turn = !turn;
-            count -= matches;
-            System.out.println("Осталось " + count + " спичек");
-        } else {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до 3:");
+            int matches = Integer.parseInt(input.nextLine());
+            if (matches >= 1 && matches <= Math.min(3, count)) {
+                turn = !turn;
+                count -= matches;
+                System.out.println("Осталось " + count + " спичек");
+             } else {
             System.out.println("неверное число!");
-        }
+            }
         }
         if (!turn) {
-        System.out.println("Выиграл первый игрок!");
+            System.out.println("Выиграл первый игрок!");
         }   else {
-        System.out.println("Выиграл второй игрок!");
+            System.out.println("Выиграл второй игрок!");
         }
     }
 }
