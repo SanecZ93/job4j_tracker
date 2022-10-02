e8adc28dda0f0db99bc5af8ae772a3419995cb7e
SanecZ93 <zyryanovalex93@gmail.com>
2022-10-02 23:05:47 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -2,35 +2,38 @@ package ru.job4j.early;
 
 public class PasswordValidator {
 
-    public static String validate(String password, String dest) {
-        if (password == null) {
-            return "password null";
-        }
-        if (password.length() < 8 || password.length() > 32) {
-            return "the password must be between 8 and 32";
-        }
-        if (!password.matches("(.*[0-9].*)")) {
-            return "The password must contain at least one digit";
-        }
-        if (!password.matches("(.*[A-Z].*)")) {
-            return "The password must contain at least one uppercase character";
-        }
-        if (!password.matches("(.*[a-z].*)")) {
-            return "The password must contain at least one lowercase character";
-        }
-        if (!password.matches("(.*[@,#,$,%].*$)")) {
-            return "The password must contain at least one special character";
-        }
-        for (int i = password.length() - dest.length(); i >= 0; i--) {
-            if (password.regionMatches(true, i, dest, 0, dest.length())) {
-                return "The password must not contain case-insensitive substrings";
+        public static String validate(String password) {
+            if (password == null) {
+                throw new IllegalArgumentException("password null");
             }
+            if (password.length() < 8 || password.length() > 32) {
+                throw new IllegalArgumentException("the password must "
+                        + "be between 8 and 32");
+            }
+            if (!password.matches("(.*[0-9].*)")) {
+                throw new IllegalArgumentException("The password must "
+                        + "contain at least one digit");
+            }
+            if (!password.matches("(.*[A-Z].*)")) {
+                throw new IllegalArgumentException("The password must "
+                        + "contain at least one uppercase character");
+            }
+            if (!password.matches("(.*[a-z].*)")) {
+                throw new IllegalArgumentException("The password must "
+                        + "contain at least one lowercase character");
+            }
+            if (!password.matches("(.*[@,#,$,%].*$)")) {
+                throw new IllegalArgumentException("The password must "
+                        + "contain at least one special character");
+            }
+            return password;
         }
-        return password;
-    }
+        public static boolean processRegionMatches(String password, String dest) {
+            for (int i = password.length() - dest.length(); i >= 0; i--) {
+                if (password.regionMatches(true, i, dest, 0, dest.length())) {
+                    throw new IllegalArgumentException("The password must not contain case-insensitive substrings");
+                }
+            }
+            return true;
+        }
 }
-
-
-
-
-
--- src/test/java/ru/job4j/early/PasswordValidatorTest.java
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -3,62 +3,82 @@ package ru.job4j.early;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
+import static ru.job4j.early.PasswordValidator.processRegionMatches;
 
 public class PasswordValidatorTest {
 
     @Test
     public void whenEmptyPasswordNull() {
-        String rsl = PasswordValidator.validate(null, null);
-        String expected = "password null";
-        assertThat(rsl).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate(null));
+        assertThat(exception.getMessage()).isEqualTo(
+                "password null");
     }
 
     @Test
     public void whenEmptyPasswordNot8to32() {
-        String rsl = PasswordValidator.validate("P@s$W1", null);
-        String expected = "the password must be between 8 and 32";
-        assertThat(rsl).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate("P@s$W1"));
+        assertThat(exception.getMessage()).isEqualTo(
+                "the password must be between 8 and 32");
     }
 
     @Test
     public void whenEmptyPasswordNotDigit() {
-        String rsl = PasswordValidator.validate("P@s$WORdd", null);
-        String expected = "The password must contain at least one digit";
-        assertThat(rsl).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate("P@s$WORdd"));
+        assertThat(exception.getMessage()).isEqualTo(
+                "The password must contain at least one digit");
     }
 
     @Test
     public void whenEmptyPasswordNotUpLitter() {
-        String rsl = PasswordValidator.validate("p@s$w0rdd1", null);
-        String expected = "The password must contain at least one uppercase character";
-        assertThat(rsl).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate("p@s$w0rdd1"));
+        assertThat(exception.getMessage()).isEqualTo(
+                "The password must contain at least one uppercase character");
     }
 
     @Test
     public void whenEmptyPasswordNotLowLitter() {
-        String rsl = PasswordValidator.validate("P@S$W0RD1", null);
-        String expected = "The password must contain at least one lowercase character";
-        assertThat(rsl).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate("P@S$W0RD1"));
+        assertThat(exception.getMessage()).isEqualTo(
+                "The password must contain at least one lowercase character");
     }
 
     @Test
     public void whenEmptyPasswordNotSpecialSymbol() {
-        String rsl = PasswordValidator.validate("PaSsW0RD1", null);
-        String expected = "The password must contain at least one special character";
-        assertThat(rsl).isEqualTo(expected);
-    }
-
-    @Test
-    public void whetContainSubstrings() {
-        String rsl = PasswordValidator.validate("123#Password#321", "Password");
-        String expected = "The password must not contain case-insensitive substrings";
-        assertThat(rsl).isEqualTo(expected);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.validate("PaSsW0RD1"));
+        assertThat(exception.getMessage()).isEqualTo(
+                "The password must contain at least one special character");
     }
 
     @Test
     public void whenValidPassword() {
-        String rsl = PasswordValidator.validate("P@s$W0Rdd1", "Password");
+        String rsl = PasswordValidator.validate("P@s$W0Rdd1");
         String expected = "P@s$W0Rdd1";
         assertThat(rsl).isEqualTo(expected);
+    }
+
+    @Test
+    public void whetNotContainSubstrings() {
+        assertTrue(processRegionMatches("P@s$W0Rdd1", "Password"));
+    }
+
+    @Test
+    public void whetContainSubstrings() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> PasswordValidator.processRegionMatches("Password123@", "Password"));
+        assertThat(exception.getMessage()).isEqualTo(
+                "The password must not contain case-insensitive substrings");
     }
 }
