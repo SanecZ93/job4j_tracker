20f99b71b5644b46e2edad85105e52c925622835
SanecZ93 <zyryanovalex93@gmail.com>
2022-10-08 14:02:29 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -2,7 +2,7 @@ package ru.job4j.early;
 
 public class PasswordValidator {
 
-    public static String validate(String password, String dest) {
+    public static String validate(String password) {
         if (password == null) {
             throw new IllegalArgumentException("password null");
         }
@@ -10,27 +10,83 @@ public class PasswordValidator {
             throw new IllegalArgumentException("the password must "
                     + "be between 8 and 32");
         }
-        if (!password.matches("(.*[0-9].*)")) {
+        if (PasswordValidator.isDigit(password)) {
             throw new IllegalArgumentException("The password must "
                     + "contain at least one digit");
         }
-        if (!password.matches("(.*[A-Z].*)")) {
+        if (PasswordValidator.upLitter(password)) {
             throw new IllegalArgumentException("The password must "
                     + "contain at least one uppercase character");
         }
-        if (!password.matches("(.*[a-z].*)")) {
+        if (PasswordValidator.lowLitter(password)) {
             throw new IllegalArgumentException("The password must "
                     + "contain at least one lowercase character");
         }
-        if (!password.matches("(.*[@,#,$,%].*$)")) {
+        if (PasswordValidator.specialSymbol(password)) {
             throw new IllegalArgumentException("The password must "
                     + "contain at least one special character");
         }
-        for (int i = password.length() - dest.length(); i >= 0; i--) {
-            if (password.regionMatches(true, i, dest, 0, dest.length())) {
-                throw new IllegalArgumentException("The password must not contain case-insensitive substrings");
+        boolean valid = password.toUpperCase().contains("PASSWORD".toUpperCase())
+                    || password.toLowerCase().contains("password".toLowerCase())
+                    || password.toUpperCase().contains("USER".toUpperCase())
+                    || password.toLowerCase().contains("user".toLowerCase())
+                    || password.toUpperCase().contains("QWERTY".toUpperCase())
+                    || password.toLowerCase().contains("qwerty".toLowerCase())
+                    || password.toUpperCase().contains("ADMIN".toUpperCase())
+                    || password.toLowerCase().contains("admin".toLowerCase())
+                    || password.contains("12345");
+            if (valid) {
+                throw new IllegalArgumentException("The password must "
+                        + "not contain case-insensitive substrings");
+            }
+            return password;
+    }
+
+    private static boolean isDigit(String password) {
+        Boolean valid = null;
+        for (int i = 0; i < password.length(); i++) {
+            int code = password.codePointAt(i);
+            valid = !Character.isDigit(code);
+            if (!valid) {
+                break;
             }
         }
-        return password;
+        return Boolean.TRUE.equals(valid);
+    }
+
+    private static boolean upLitter(String password) {
+        Boolean valid = null;
+        for (int i = 0; i < password.length(); i++) {
+            int code = password.codePointAt(i);
+            valid = !Character.isUpperCase(code);
+            if (!valid) {
+                break;
+            }
+        }
+        return Boolean.TRUE.equals(valid);
+    }
+
+    private static boolean lowLitter(String password) {
+        Boolean valid = null;
+        for (int i = 0; i < password.length(); i++) {
+            int code = password.codePointAt(i);
+            valid = !Character.isLowerCase(code);
+            if (!valid) {
+                break;
+            }
+        }
+        return Boolean.TRUE.equals(valid);
+    }
+
+    private static boolean specialSymbol(String password) {
+        Boolean valid = null;
+        for (int i = 0; i < password.length(); i++) {
+            int code = password.codePointAt(i);
+            valid = !(code == 36 || code == 95);
+            if (!valid) {
+                break;
+            }
+        }
+        return Boolean.TRUE.equals(valid);
     }
 }
