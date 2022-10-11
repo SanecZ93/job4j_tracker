2312abe9966e617557beb9ee9fe141a5789d64dd
SanecZ93 <zyryanovalex93@gmail.com>
2022-10-11 17:51:52 +0300

    1. Singleton [#94606 #350538]

--- src/main/java/ru/job4j/SingleTracker.java
+++ src/main/java/ru/job4j/SingleTracker.java
@@ -23,5 +23,23 @@ public class SingleTracker {
         return tracker.add(item);
     }
 
-    /* Остальные методы из класса Tracker. */
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String key) {
+      return tracker.findByName(key);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
 }
