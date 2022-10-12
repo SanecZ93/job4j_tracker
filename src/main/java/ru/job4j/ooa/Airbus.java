9e6866339c66720c268ec133ab1dbe6f3f4514c6
SanecZ93 <zyryanovalex93@gmail.com>
2022-10-12 21:35:46 +0300

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,42 @@
+package ru.job4j.ooa;
+
+import java.util.Objects;
+
+final public class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        int OTHER_ENGINE = 4;
+        if (Objects.equals(name, "A380")) {
+            System.out.println("Количество двигателей равно: " + 4);
+        } else {
+            System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
