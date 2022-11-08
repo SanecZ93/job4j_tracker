3766fc0b71e90abfa84737536fa849ce20687179
SanecZ93 <zyryanovalex93@gmail.com>
2022-11-08 19:57:59 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ItemAscByNameTest {
+
+    @Test
+    public void whenAverageScore() {
+        List<Item> items = new ArrayList<>(List.of(new Item("Pavel"),
+                new Item("Alex"),
+                new Item("Ivan"),
+                new Item("Vlad"))
+        );
+        items.sort(new ItemAscByName());
+        List<Item> expected = new ArrayList<>(List.of(new Item("Alex"),
+                new Item("Ivan"),
+                new Item("Pavel"),
+                new Item("Vlad"))
+        );
+        assertThat(items.toString()).isEqualTo(expected.toString());
+    }
+}
