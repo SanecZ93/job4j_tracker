5618f28b05fb00892a5321c3266a021d61b8f15c
SanecZ93 <zyryanovalex93@gmail.com>
2022-11-10 14:56:31 +0300

    0. Сортировка [#10096 #359065]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ItemAscByNameTest {
+public class ItemTest {
 
     @Test
-    public void whenAverageScore() {
+    public void whenSortAscByName() {
         List<Item> items = new ArrayList<>(List.of(new Item("Pavel"),
                 new Item("Alex"),
                 new Item("Ivan"),
@@ -19,6 +19,22 @@ public class ItemAscByNameTest {
                 new Item("Ivan"),
                 new Item("Pavel"),
                 new Item("Vlad"))
+        );
+        assertThat(items).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenSortDescByName() {
+        List<Item> items = new ArrayList<>(List.of(new Item("Pavel"),
+                new Item("Alex"),
+                new Item("Ivan"),
+                new Item("Vlad"))
+        );
+        items.sort(new ItemDescByName());
+        List<Item> expected = new ArrayList<>(List.of(new Item("Vlad"),
+                new Item("Pavel"),
+                new Item("Ivan"),
+                new Item("Alex"))
         );
         assertThat(items).isEqualTo(expected);
     }
