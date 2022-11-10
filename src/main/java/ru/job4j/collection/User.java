2dd62828b423b55379b9dc6eabc3a4c9161282f5
SanecZ93 <zyryanovalex93@gmail.com>
2022-11-10 14:48:47 +0300

    1. Организовать сортировку User [#10034 #359086]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -14,9 +14,8 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        return (name.equals(o.name)
-                ? Integer.compare(age, o.age)
-                : this.name.compareTo(o.name));
+        int rsl = this.name.compareTo(o.name);
+        return rsl != 0 ? rsl : Integer.compare(age, o.age);
     }
 
     @Override
