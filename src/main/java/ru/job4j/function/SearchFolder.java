7a4cf88984526d3a4aff9cc6868c628ac8c912db
SanecZ93 <zyryanovalex93@gmail.com>
2022-11-11 20:42:47 +0300

    Revert "2.3. Встроенные функциональные интерфейсы. [#249209 #366896]"
    This reverts commit 8e280a063fb5359dfcc8063bbf0ad19a20cb3380.

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -7,9 +7,10 @@ import java.util.function.Predicate;
 public class SearchFolder {
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
         ArrayList<Folder> rsl = new ArrayList<>();
-        for (Folder lst : list) {
-           if (pred.test(lst)) {
-               rsl.add(lst);
+        pred = s -> s.getName().contains("bug") || s.getSize() > 100;
+        for (Folder s : list) {
+           if (pred.test(s)) {
+               rsl.add(s);
            }
         }
        return rsl;
